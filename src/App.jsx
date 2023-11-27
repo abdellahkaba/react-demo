ddb56462782a6a9dd6178ed223ab3f46923a5522
Abdellah <abdallahkaba98@gmail.com>
2023-11-27 06:39:32 +0000

    UseEffect cas 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,49 @@
-import { useState } from 'react'
-import { Input } from './components/forms/input'
-import { CheckBox } from './components/forms/checkbox'
-import { ProductCategoryRow } from './components/products/productCategoryRow'
-import { ProductRow } from './components/products/productRow'
+import { useEffect, useState } from "react"
+import { Input } from "./components/forms/input"
 
-
-const PRODUCTS = [  
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},  
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},  
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},  
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},  
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},  
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}  
-]
-console.log(PRODUCTS)
-console.log(PRODUCTS[3].name)
 
 function App() {
 
-  const [showStockedOnly,setShowStockedOnly] = useState(false)
-  const [search, setSearch] = useState('')
-  /**Une constante qui permet de filtrer les produits */
-  const visibleProducts = PRODUCTS.filter(product =>{
-    if(showStockedOnly && !product.stocked){
-      return false
+    const [duration,setDuration] = useState(5)
+    /**une nouvelle constante qui permet de savoir ou on en est */
+    const [secondLeft,setSecondLeft] = useState(duration)
+
+    /**Nb
+     * quant on utilise useEffect, il ne faut utiliser aucune variable de composant qui son en dehors du effect
+     * si on utilise seulement setTime dans une fonction de useEffect passons à faire un nettoyage de cet timer
+     * ne met jamais setFonction à la racine de useEffect
+     */
+
+    /** La solution c'est de créer un handler pour le changement de la valeur initiale de  */
+
+    const handlerChange = (v) =>{
+        setDuration(v)
+        setSecondLeft(v)
     }
 
-    if(search && !product.name.includes(search)) {
-      return false
-    }
+    console.log("Bien !")
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSecondLeft(v => v <= 1 ? 0 : v - 1)
+        }, 1000)
+        /**Nettoyer le precedent timer */
+        return () => {
+            clearInterval(timer)
+        }
+    }, [duration])
 
-    return true
-  })
-return (
-  <>
-    <div className='container my-3'>
-        <SearcheBar 
-        showStockedOnly={showStockedOnly} 
-        onStockedOnlyChange={setShowStockedOnly} 
-        search={search}
-        onSearchChange={setSearch}
-        />
-        <ProductTable products={visibleProducts}></ProductTable>
-    </div>
-  </>
-)
-}
-
-function SearcheBar({showStockedOnly,onStockedOnlyChange,search,onSearchChange}) {
-  return <div>
-    <div className='mb-3'>
-      <Input 
-      value={search} 
-      onChange={onSearchChange} 
-      placeholder="Rechercher..." />
-      <input type='range' className='form-range' min={0} max={10} />
-      <Input 
-      value={search} 
-      onChange={onSearchChange} 
-      placeholder="Nom de Kaba Abdoulaye..." />
-      <CheckBox 
-        id="stocked" 
-        checked={showStockedOnly}  
-        onChange={onStockedOnlyChange} 
-        label="N'affichez que les elements du produits" />
-    </div>
-  </div>
-}
-
-/**
- * Un composant qui va regrouper tous les autres composants
- * cette fonction va recevoir la liste des produits
- */
-function ProductTable({products}) {
-
-    /**un tableau qui va contenir les composants */
-    const row = []
-    /**une variable qui prendra la precedente category */
-    let lastCategory = null
-    for(let product of products) {
-      if(product.category !== lastCategory) {
-
-        /**Lorsqu'on on utilise un tableau les élements doivent avoir les clé (key) */
-        row.push(<ProductCategoryRow key={product.category} name={product.category} />)
-      }
-
-      lastCategory = product.category
-      row.push(<ProductRow product={product} key={product.name} />)
-    }
-    
     return <>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Nom</th>
-              <th>Prix</th>
-            </tr>
-          </thead>
-          <tbody>
-            {row}
-          </tbody>
-        </table>
+        <div className="py-2">
+            <Input 
+            value={duration} 
+            onChange={handlerChange} 
+            placeholder="Saisir une valeur" />
+        </div>
+
+        <p>
+            Decompte : {secondLeft}
+        </p>
     </>
 }
 
 export default App
-
-
