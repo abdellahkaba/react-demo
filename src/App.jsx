03b9d8c846b39d3ebefca89959d18c1c489498cc
Abdellah <abdallahkaba98@gmail.com>
2023-11-28 14:03:11 +0000

    useRef cas 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import { Input } from "./components/forms/input"
 
 function App() {
 
-    const ref = useRef()
+    const ref = useRef(null)
     
     console.log('Application', ref)
-
-    return <div>
-        <Input inputRef={ref} label="prefix" />
+    const [value,setValue] = useState('')
+    return <div ref={ref}>
+        <Input ref={ref}
+         label="prefix"
+         value={value}
+         onChange={setValue}
+         />
+         {value.length === 0 && <div ref={ref}>hello</div>}
     </div>
 }
 
--- src/components/forms/input.jsx
+++ src/components/forms/input.jsx
@@ -6,26 +6,43 @@
  * @returns 
  */
 
-import { useId } from "react"
+import { forwardRef, useId } from "react"
 
 // import { forwardRef, useId } from "react"
 
-export function Input({placeholder, value, label, onChange, inputRef,labelRef,divRef}) {
-    const id = useId()
-    console.log('input', inputRef)
-    console.log('label', labelRef)
-    console.log('div', divRef)
-    return <>
-        <div ref={divRef}>
-            <label ref={labelRef} className="form-label" htmlFor={id}>{label}</label>
-            <input type="text"
-                ref={inputRef}
-                className="form-control"
-                value={value}
-                placeholder={placeholder}
-                onChange={(e) => onChange(e.target.value)}
-                />
-        </div>
-    </>
-}
+// export function Input({placeholder, value, label, onChange, inputRef,labelRef,divRef}) {
+//     const id = useId()
+//     console.log('input', inputRef)
+//     console.log('label', labelRef)
+//     console.log('div', divRef)
+//     return <>
+//         <div ref={divRef}>
+//             <label ref={labelRef} className="form-label" htmlFor={id}>{label}</label>
+//             <input type="text"
+//                 ref={inputRef}
+//                 className="form-control"
+//                 value={value}
+//                 placeholder={placeholder}
+//                 onChange={(e) => onChange(e.target.value)}
+//                 />
+//         </div>
+//     </>
+// }
 
+export const Input = forwardRef(function Input({placeholder, value, onChange,label}, ref) {
+    const id = useId()
+    return <>
+    <div>
+    <label className="form-label" htmlFor={id}>{label}</label>
+        <input 
+            id={id}
+            ref={ref}
+            type="text"
+            className="form-control"
+            value={value}
+            placeholder={placeholder}
+            onChange={(e) => onChange(e.target.value)}
+            />
+    </div>
+</>
+})
