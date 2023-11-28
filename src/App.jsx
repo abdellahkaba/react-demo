9f6f6f470b9cb1a489a25951e82f8f4a798bbf4d
Abdellah <abdallahkaba98@gmail.com>
2023-11-28 13:23:36 +0000

    useRef cas 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Input } from "./components/forms/input"
 
 
 function App() {
 
-    const [duration,setDuration] = useState(5)
-    /**une nouvelle constante qui permet de savoir ou on en est */
-    const [secondLeft,setSecondLeft] = useState(duration)
+    const ref = useRef()
+    
+    console.log('Application', ref)
 
-    /**Nb
-     * quant on utilise useEffect, il ne faut utiliser aucune variable de composant qui son en dehors du effect
-     * si on utilise seulement setTime dans une fonction de useEffect passons à faire un nettoyage de cet timer
-     * ne met jamais setFonction à la racine de useEffect
-     */
-
-    /** La solution c'est de créer un handler pour le changement de la valeur initiale de  */
-
-    const handlerChange = (v) =>{
-        setDuration(v)
-        setSecondLeft(v)
-    }
-
-    console.log("Bien !")
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setSecondLeft(v => {
-                if(v <=1) {
-                    clearInterval(timer)
-                    return 0
-                }
-                return v - 1
-            })
-        }, 1000)
-        /**Nettoyer le precedent timer */
-        return () => {
-            clearInterval(timer)
-        }
-    }, [duration])
-
-    return <>
-        <div className="py-2">
-            <Input 
-            value={duration} 
-            onChange={handlerChange} 
-            placeholder="Saisir une valeur" />
-        </div>
-
-        <p>
-            Decompte : {secondLeft}
-        </p>
-    </>
+    return <div>
+        <Input inputRef={ref} label="prefix" />
+    </div>
 }
 
 export default App
--- src/components/forms/input.jsx
+++ src/components/forms/input.jsx
@@ -6,12 +6,20 @@
  * @returns 
  */
 
+import { useId } from "react"
+
 // import { forwardRef, useId } from "react"
 
-export function Input({placeholder, value, onChange}) {
+export function Input({placeholder, value, label, onChange, inputRef,labelRef,divRef}) {
+    const id = useId()
+    console.log('input', inputRef)
+    console.log('label', labelRef)
+    console.log('div', divRef)
     return <>
-        <div>
+        <div ref={divRef}>
+            <label ref={labelRef} className="form-label" htmlFor={id}>{label}</label>
             <input type="text"
+                ref={inputRef}
                 className="form-control"
                 value={value}
                 placeholder={placeholder}
@@ -21,19 +29,3 @@ export function Input({placeholder, value, onChange}) {
     </>
 }
 
-// export const Input = forwardRef(function Input({placeholder, value, onChange}, ref) {
-//     const id = useId()
-//     return <>
-//     <div>
-//         <input 
-//             id={id}
-//             ref={ref}
-//             type="text"
-//             className="form-control"
-//             value={value}
-//             placeholder={placeholder}
-//             onChange={(e) => onChange(e.target.value)}
-//             />
-//     </div>
-// </>
-// })
