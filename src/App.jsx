adb97a57d6b9fed0c3426bd523664bea43844456
Abdellah <abdallahkaba98@gmail.com>
2023-11-29 09:24:21 +0000

    hook personalisé 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useToggle } from "./hooks/useToggle"
+import { useIncrement } from "./hooks/useIncrement"
 /**Exemple d'utilisation de hook personalisé */
 
 function App() {
     /**Appel de la fonction hook personalisée */
-    const [checked, toggleCheck] = useToggle()
+    const [value,increment,desincrement] = useIncrement(0)
     return <>
-         <div className="text-center w-2 py-2">
-        <input type="checkbox" checked={checked} onChange={toggleCheck} />
-        { checked && 'Je suis coché'}
+         <div className="text-center w-2 py-2 ">
+         Compteur : {value}
+         <button className="btn btn-primary m-2" onClick={increment}>Incrementation</button>
+         <button className="btn btn-danger" onClick={desincrement}>Desincrementation</button>
     </div>
+    
+    </>
 
 }
 
